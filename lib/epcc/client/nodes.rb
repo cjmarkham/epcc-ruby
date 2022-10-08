d6cbf921b3fafdeb4dd88b2fcfd848af6a6e4b62
Carl Markham <carl.markham@elasticpath.com>
2022-10-08 13:46:57 +0100

    allow attaching products to a node

--- lib/epcc/client/nodes.rb
+++ lib/epcc/client/nodes.rb
@@ -27,6 +27,62 @@ module EPCC
       def delete_node(hierarchy_id, node_id)
         delete("/pcm/hierarchies/#{hierarchy_id}/nodes/#{node_id}")
       end
+
+      # Get a node's products
+      #
+      # @param hierarchy_id [String] The UUID of the hierarchy
+      # @param node_id [String] The UUID of the node
+      def get_node_products(hierarchy_id, node_id)
+        get("/pcm/hierarchies/#{hierarchy_id}/nodes/#{node_id}/products")
+      end
+
+      # Attach product(s) to a node
+      # This method accepts any number of arguments after the node UUID,
+      # each representing a different product UUID
+      #
+      # @param hierarchy_id [String] The UUID of the hierarchy
+      # @param node_id [String] The UUID of the node
+      # @param product_ids [String] The UUID of the product
+      def attach_products_to_node(hierarchy_id, node_id, *product_ids)
+        data = []
+
+        (product_ids || []).each do |id|
+          data.push({
+            type: 'product',
+            id: id,
+          })
+        end
+
+        post("/pcm/hierarchies/#{hierarchy_id}/nodes/#{node_id}/relationships/products", {
+          body: {
+            data: data,
+          }
+        })
+      end
+
+      # Removes product(s) from a node
+      # This method accepts any number of arguments after the node UUID,
+      # each representing a different product UUID
+      #
+      # @param hierarchy_id [String] The UUID of the hierarchy
+      # @param node_id [String] The UUID of the node
+      # @param product_ids [String] The UUID of the product
+      def remove_products_from_node(hierarchy_id, node_id, *product_ids)
+        data = []
+
+        (product_ids || []).each do |id|
+          data.push({
+            type: 'product',
+            id: id,
+          })
+        end
+
+        delete("/pcm/hierarchies/#{hierarchy_id}/nodes/#{node_id}/relationships/products", {
+          body: {
+            data: data,
+          }
+        })
+      end
     end
   end
 end
